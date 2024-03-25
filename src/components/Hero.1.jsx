be47548978d22d572cc63fe138ca1c2d003bed11
jenniferagbeli <agbelij12@gmail.com>
2024-03-25 09:55:26 +0000

    first commit-1

--- src/components/Hero.1.jsx
+++ src/components/Hero.1.jsx
@@ -4,11 +4,11 @@ export default function Hero() {
     return (
         <div className="relative">
             <div className="intro-header bg-cover bg-center bg-fixed" style={{ backgroundImage: "url('https://40.media.tumblr.com/1b916597d3e174399cb7adadddb66ede/tumblr_nt5uk4psl31ud7rr3o1_1280.jpg')", height: "500px" }}>
-                <div className="absolute inset-0 bg-black opacity-20"></div>
+                <div className="absolute inset-0 bg-black opacity-15"></div>
                 <div className="container mx-auto">
                     <div className="text-center text-white">
                         <h1 className="text-5xl font-bold pt-32 mb-4">Topaz Web Design Studio</h1>
-                        <h3 className="text-xl text-bold text-gray-600 text-shadow">Where Awesomeness Is Brought To Life.</h3>
+                        <h3 className="text-2xl text-bold text-gray-800 text-shadow">Where Awesomeness Is Brought To Life.</h3>
                         <hr className="w-1/3 border-t border-gray-200 border-b border-gray-200 mx-auto" />
                         <ul className="list-inline intro-social-buttons">
                             <li>
