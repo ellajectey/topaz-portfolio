6911e3191d843a8f1c06a0086b860be6d5f1902f
Melomey <samuelmelomey@gmail.com>
2024-03-26 05:59:24 +0000

    added card componenets to project page

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import NavBar from '../components/navbar';
-import Experience from '../components/experience';
-import Achievement from '../components/achievement';
-import Footer from '../components/footer'
+import React from "react";
+import NavBar from "../components/navbar";
+import Experience from "../components/experience";
+import Achievement from "../components/achievement";
+import Footer from "../components/footer";
+import Projectpost from "../components/projectpost";
 
 function Project() {
-    return (
-        <>
-         <NavBar/>
-            <Experience/>
-            <Achievement/>
-            <Footer/>   
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <Experience />
+      <Achievement />
+      <Projectpost />
+      <Footer />
+    </>
+  );
 }
 
 export default Project;
