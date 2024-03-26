f9099d98e25eae718f1b337caf64491d6e01e8b1
jenniferagbeli <agbelij12@gmail.com>
2024-03-26 01:56:04 +0000

    third commit

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function Skill() {
     return (
         <section className="py-16 bg-gray-100 ">
@@ -44,27 +45,27 @@ function Skill() {
                         <h3 className="text-xl font-semibold mb-4">Tools & expertise:</h3>
                         <p className="mb-4">Here are the tools and areas in which I have expertise:</p>
                         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                            <a href="/blog" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="/about" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Git</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">Version control system for tracking changes in code.</p>
                             </a>
-                            <a href="/blog" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="/about" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Responsive Web Design</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">Design approach aimed at creating websites that adapt to various screen sizes.</p>
                             </a>
-                            <a href="/blog" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="/about" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jira</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">Project management tool for tracking and managing software development projects.</p>
                             </a>
-                            <a href="/blog" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="/about" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Google Tools</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">Suite of productivity tools including Gmail, Google Drive, and Google Docs.</p>
                             </a>
-                            <a href="/blog" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="/about" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tailwind CSS</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">A utility-first CSS framework for creating custom designs with ease.</p>
                             </a>
-                            <a href="/blog" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="/about" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Material UI</h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">A React UI framework based on Google's Material Design.</p>
                             </a>
