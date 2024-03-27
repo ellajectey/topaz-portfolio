adc32d427ee4cf8f998792c6ea8f548419671ae2
jenniferagbeli <agbelij12@gmail.com>
2024-03-27 14:43:37 +0000

    fifth commit

--- src/components/Hero.1.jsx
+++ src/components/Hero.1.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Hero() {
     return (
-        <div className="relative mb-10">
-            <div className="intro-header bg-cover bg-center" style={{ backgroundImage: "url('https://40.media.tumblr.com/1b916597d3e174399cb7adadddb66ede/tumblr_nt5uk4psl31ud7rr3o1_1280.jpg')", height: "550px" }}>
-                <div className="absolute inset-0 bg-black opacity-10"></div>
-                <div className="container mx-auto mt-10 ">
-                    <div className="text-center text-white">
-                        <h1 className="text-5xl font-bold pt-32 mb-6">Topaz Web Design Studio</h1>
-                        <h3 className="text-2xl text-bold text-gray-800 text-shadow">Where Awesomeness Is Brought To Life.</h3>
+        <div className="relative">
+            <div className="intro-header bg-gray-200">
+                <div className="p-10 container mx-auto mt-4 top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
+                <div className="text-center text-white bg-yellow-900 bg-opacity-50 p-4 mt-4 md:p-8 rounded-md">
+                        <h1 className="text-5xl font-bold pt-32 pt-5">Topaz Web Design Studio</h1>
+                        <h3 className="text-2xl text-bold text-gray-800 text-shadow mt-8">Where Awesomeness Is Brought To Life.</h3>
                         <hr className="w-1/3 border-t border-gray-200 border-b border-gray-200 mx-auto mb-4" />
-                        <ul className="list-inline intro-social-buttons text-center text-bold text-yellow-900 flex justify-center">
-                            <li>
-                                <a href="https://twitter.com/Ferreir4Thiago" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-square-x-twitter"></i><span className="network-name">Twitter</span></a>
+                        <ul className="list-inline intro-social-buttons text-bold text-white flex justify-center">
+                            <li className='mr-4'>
+                                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-square-x-twitter"></i><span className="network-name">Twitter</span></a>
                             </li>
-                            <li>
-                                <a href="https://github.com/ThiagoFerreir4" target="_blank" rel="noopener noreferrer" ><i class="fa-brands fa-square-github"></i> <span className="network-name">Github</span></a>
+                            <li className='mr-4'>
+                                <a href="https://www.github.com/" target="_blank" rel="noopener noreferrer" ><i className="fa-brands fa-square-github"></i> <span className="network-name">Github</span></a>
                             </li>
-                            <li>
-                                <a href="https://www.linkedin.com/in/thiagoferreir4" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i><span className="network-name">Linkedin</span></a>
+                            <li className='mr-4'>
+                                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i><span className="network-name">Linkedin</span></a>
                             </li>
-                            <li>
-                                <a href="https://www.freecodecamp.com/thiagoferreira" target="_blank" rel="noopener noreferrer"><i className="fa fa-free-code-camp fa-fw"></i> <span className="network-name">freeCodeCamp</span></a>
+                            <li className='mr-4'>
+                                <a href="https://www.figma.com/" target="_blank" rel="noopener noreferrer"><i className="fa fas fa-cogs"></i> <span className="network-name">Figma</span></a>
                             </li>
                         </ul>
+
+                        <Link to="/about" className="hire mt-10 text-lg bg-yellow-800 text-bold text-black py-2 px-6 rounded-full inline-block transition duration-500 ease-in-out hover:bg-gray-300 hover:border-black border hover:border-solid border-black">Get to know us</Link>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
