1d404c5a582d3db9bfde4dbde3aa439122c5ae85
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-03-28 07:55:19 +0000

    final remote changes to portfolio app

--- src/components/Hero.1.jsx
+++ src/components/Hero.1.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import backgroundImage from '../assets/images/homevideo.gif'; // Import your GIF file
 
 export default function Hero() {
     return (
-        <div className="relative">
-            <div className="intro-header bg-gray-400 py-16">
-                <div className="p-10 container mx-auto mt-4 top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
+        <div >
+            <div className="bg-cover" style={{backgroundImage: `url(${backgroundImage})`, height: '100vh', overflow: 'hidden'}}>
+                <div className="p-10 mx-auto mt-4 top-0 left-0 right-0 bottom-0  flex-col justify-center items-center">
                     <div className="text-center text-white bg-yellow-900 bg-opacity-20 p-4 mt-4 md:p-8 rounded-md">
-                        <h1 className="text-5xl font-bold pt-32 pt-5 animate-bounce">Hello!</h1>
+                        <h1 className="text-5xl font-bold pt-32 pt-5 ">Hello!</h1>
                         <h3 className="text-2xl text-bold text-black text-shadow mt-4">🌟 Welcome to Topaz Portfolio</h3>
-                        <h4 className="text-2xl text-bold text-shadow mt-2 text-yellow-900">Where Awesomeness Is Brought To Life!💻</h4>
+                        <h4 className="text-2xl text-bold text-shadow mt-2 text-white">Where Awesomeness Is Brought To Life!💻</h4>
                         <hr className="w-3/4 border-t border-gray-200 border-b border-gray-200 mx-auto mb-4" />
                         <ul className="list-inline intro-social-buttons text-bold text-white flex justify-center">
                             <li className='mr-4'>
